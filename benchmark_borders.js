16a0b746c593606af0fa6a5da8d8913ff7a53d15
Pattrick Hueper <phueper@hueper.net>
2018-10-30 21:55:38 +0100

    try a borders func benchmark test

---
+++ benchmark_borders.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+'use strict';
+
+const Benchmark = require('benchmark')
+const beautifyBenchmark = require('beautify-benchmark')
+
+const Context = require('borders').default;
+
+const value = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+function* generatorFunc() {
+  for (let i = 0; i < value.length; i += 1) {
+    yield value[i]
+  }
+}
+
+const bordersContext = new Context()
+  .use({
+    FUNC: (index) => {
+      // console.log('in backend', index);
+      return value[index]
+    }
+  })
+
+// add tests
+new Benchmark.Suite()
+  .add('plain generator function', function () {
+    const gen = generatorFunc()
+    let genValue = gen.next()
+    const val = []
+    while (!genValue.done) {
+      val.push(genValue.value)
+      genValue = gen.next()
+    }
+    // console.log('generator returned', val)
+  })
+  .add('function through borders',
+    {
+      defer: true,
+      fn: function (deferred) {
+        const execute = bordersContext.execute(function* run() {
+          const val = []
+          for (let i = 0; i < value.length; i += 1) {
+            val.push(yield { type: 'FUNC', payload: i })
+          }
+          return val
+        }())
+        execute.then(
+          (v) => {
+            // console.log('execute resolved', v)
+            deferred.resolve()
+          },
+          (e) => {
+            console.log('execute rejected', e)
+            deferred.resolve()
+          }
+        )
+      }
+    })
+  // add listeners
+  .on('start', function () {
+    console.log('Starting tests...');
+  })
+  .on('cycle', function (event) {
+    // console.log(String(event.target));
+    beautifyBenchmark.add(event.target);
+  })
+  .on('complete', function () {
+    beautifyBenchmark.log()
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  // run async
+  .run({ 'async': true });
